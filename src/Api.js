9e7bae1df6648e729f71fab459b9d9a7b4d9c64a
Sam Merham <sammerham-rithm@sams-mbp.lan>
2021-06-01 12:31:45 -0700

    worked on helperApi and routes

---
+++ src/Api.js
@@ -0,0 +1,88 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
+/** API Class.
+ *
+ * Static class tying together methods used to get/send to to the API.
+ * There shouldn't be any frontend-specific stuff here, and there shouldn't
+ * be any API-aware stuff elsewhere in the frontend.
+ *
+ */
+
+class JoblyApi {
+  // the token for interactive with the API will be stored here.
+  static token;
+
+  static async request(endpoint, data = {}, method = "get") {
+    console.debug("API Call:", endpoint, data, method);
+
+    const url = `${BASE_URL}/${endpoint}`;
+    const headers = { Authorization: `Bearer ${JoblyApi.token}` };
+    const params = (method === "get")
+        ? data
+        : {};
+
+    try {
+      return (await axios({ url, method, data, params, headers })).data;
+    } catch (err) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  // Individual API routes
+
+  /** Get details on a company by handle. */
+
+  static async getCompany(handle) {
+    let res = await this.request(`companies/${handle}`);
+    return res.company;
+  };
+
+ /** Get a list of all companies. */
+
+ static async getAllCompanies() {
+  let res = await this.request(`companies/`);
+  return res.companies;
+};
+
+ /** Get a list of all companies by serach term. */
+
+ static async getCompaniesBySearchTerm(searchTerm) {
+  let res = await this.request(`companies/?nameLike=${searchTerm}`);
+  return res.companies;
+};
+
+
+/** Get details on a job by ID. */
+
+  static async getJob(id) {
+    let res = await this.request(`jobs/${id}`);
+    return res.job;
+  };
+
+
+ /** Get a list of all jobs. */
+
+ static async getAllJobs() {
+  let res = await this.request(`jobs/`);
+  return res.jobs;
+  };
+
+
+ /** Get a list of all jobs by search term. */
+
+  static async getjobsBySearchTerm(searchTerm) {
+    let res = await this.request(`jobs/?title=${searchTerm}`);
+    return res.jobs;
+};
+
+
+}
+
+// for now, put token ("testuser" / "password" on class)
+JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
+    "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
+    "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
---
+++ src/Routes.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Route, Switch, Redirect} from 'react-router-dom';
+import Homepage from './HomePage';
+import CompanyList from './CompanyList';
+import CompanyDetail from './CompanyDetail';
+import JobList from './JobList';
+import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
+import ProfileForm from './ProfileForm';
+
+
+
+function Routes(){
+    return (
+        <Switch>
+            <Route exact path="/">
+                <Homepage />
+            </Route>
+            <Route exact path="/companies">
+                <CompanyList />
+            </Route>
+            <Route exact path="/companies/:handle">
+                <CompanyDetail />
+            </Route>
+            <Route exact path="/jobs">
+                <JobList />
+            </Route>
+            <Route exact path="/login">
+                <LoginForm />
+            </Route>
+            <Route exact path="/signup">
+                <SignupForm />
+            </Route>
+            <Route exact path="/profile">
+                <ProfileForm />
+            </Route>
+            <Redirect to="/" />
+        </Switch>
+    )
+}
+
+export default Routes;
